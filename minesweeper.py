d137c190a62c211e5d5c72198fa461cfb0607dd8
gitgutm8 <clemensring@gmail.com>
2017-10-21 01:51:13 +0200

    Grafischer output

--- minesweeper.py
+++ minesweeper.py
@@ -26,11 +26,14 @@ class MineSweeper:
         self.first_click = True
 
     def _generate_mines(self, unavailable):
+        self.hints = [[0] * self.columns for _ in range(self.rows)]
         cells = list(range(self.columns * self.rows))
         cells.remove(unavailable)
         for cell_num in random.choices(cells, k=self.mines):
             row, column = divmod(cell_num, self.columns)
             self.cells[row][column] = CellState.MINED
+            for x, y in self._get_neighbors(column, row):
+                self.hints[y][x] += 1
 
     def reveal_click(self, col, row):
         if self.first_click:
@@ -44,12 +47,11 @@ class MineSweeper:
 
     def _open(self, col, row):
         self[row][col] |= CellState.OPEN
-        if self[row][col] == CellState.MINED:
+        if self[row][col] == CellState.OPEN_MINE:
             self.alive = False
             return
-        neighbors = self._get_neighbors(col, row)
-        if not any(self[ny][nx] is CellState.MINED for nx, ny in neighbors):
-            for neighbor in neighbors:
+        if self.get_hint((col, row)) == 0:
+            for neighbor in self._get_neighbors(col, row):
                 self.reveal_click(*neighbor)
 
     def _get_neighbors(self, col, row):
@@ -70,7 +72,7 @@ class MineSweeper:
 
     def flag_click(self, col, row):
         state = self[row][col]
-        if state == CellState.OPEN:
+        if state & CellState.OPEN == CellState.OPEN:
             return
         self[row][col] ^= CellState.FLAGGED
         if state == CellState.MINED:
@@ -84,6 +86,10 @@ class MineSweeper:
     def is_lost(self):
         return not self.alive
 
+    def get_hint(self, pos):
+        x, y = pos
+        return self.hints[y][x]
+
     def enumerate(self):
         for y in range(self.rows):
             for x in range(self.columns):
@@ -95,26 +101,58 @@ class MineSweeper:
 
 class MineSweeperDrawer:
 
-    def __init__(self, state_to_color, grid_line_color, game):
+    def __init__(self, state_to_color, grid_line_color, crosshair_color, font, num_colors, game):
         self.state_to_color = state_to_color
         self.grid_line_color = grid_line_color
+        self.crosshair_color = crosshair_color
         self.screen = game.screen
         self.game = game
+        self.hint_surfaces = [font.render(str(num), 1, color)
+                              for num, color
+                              in zip(range(1, 10), num_colors)]
 
     def _block(self, pos):
         return pg.Rect(pos * self.game.block_size, (self.game.block_size, self.game.block_size))
 
-    def _draw_grid_lines(self):
-        pass
-
-    def __call__(self):
+    def _draw_cells(self):
         for pos, state in self.game.board.enumerate():
             pg.draw.rect(
                 self.screen,
                 self.state_to_color[state],
                 self._block(pos)
             )
+            if state == CellState.OPEN:
+                self._draw_hint(pos)
+
+    def _draw_hint(self, pos):
+        hint = self.game.board.get_hint(pos)
+        if hint == 0: return
+        surf = self.hint_surfaces[hint-1]
+        self.screen.blit(surf, pos * self.game.block_size)
+
+    def _draw_grid_lines(self):
+        for i in range(self.game.board.columns):
+            start = i * self.game.block_size, 0
+            end = i * self.game.block_size, self.game.board.rows * self.game.block_size
+            pg.draw.line(self.screen, self.grid_line_color, start, end)
+        for i in range(self.game.board.rows):
+            start = 0, i * self.game.block_size
+            end = self.game.board.columns * self.game.block_size, i * self.game.block_size
+            pg.draw.line(self.screen, self.grid_line_color, start, end)
+
+    def _draw_crosshair(self):
+        center = self.game.block_size * self.game.selected_mine.get() + Vector2([self.game.block_size] * 2) // 2
+        pg.draw.circle(
+            self.screen,
+            self.crosshair_color,
+            center,
+            2
+        )
+
+    def __call__(self):
+        self._draw_cells()
         self._draw_grid_lines()
+        self._draw_crosshair()
 
 
 class _2dSelector:
@@ -136,7 +174,7 @@ class _2dSelector:
 
 class MineSweeperGame:
 
-    FPS = 60
+    FPS = 25
     SCREEN_WIDTH, SCREEN_HEIGHT = 1600, 900
     SCREEN_SIZE = Vector2(SCREEN_WIDTH, SCREEN_HEIGHT)
 
@@ -154,6 +192,16 @@ class MineSweeperGame:
                 CellState.OPEN_MINE: pg.Color('darkred')
             },
             grid_line_color=pg.Color('black'),
+            crosshair_color=pg.Color('blue'),
+            font=pg.font.SysFont('verdana.ttf', block_size),
+            num_colors=[(  0,  65, 170),  # blue
+                        ( 28, 122,   0),  # green
+                        (183,  25,  25),  # red
+                        (  3,  14,  76),  # blue
+                        ( 76,   3,   3),  # darkred
+                        ( 6, 111, 124),  # cyan
+                        ( 10,  10,  10),  # black
+                        (171, 186, 188)], # gray
             game=self
         )
         self.block_size = block_size
@@ -162,6 +210,8 @@ class MineSweeperGame:
         self.selected_mine = _2dSelector(Vector2(0, 0), Vector2(columns, rows))
 
     def game_loop(self):
+        self.drawer()
+        pg.display.update()
         running = True
         while running:
             self.clock.tick(self.FPS)
@@ -181,8 +231,9 @@ class MineSweeperGame:
                         self.board.flag_click(*self.selected_mine.get())
                     elif ev.key == pg.K_ESCAPE:
                         running = False
-            self.drawer()
-            pg.display.update()
+                    self.drawer()
+                    pg.display.update()
 
 if __name__ == '__main__':
-    MineSweeperGame(10, 10, 16, 30).game_loop()
+    pg.init()
+    MineSweeperGame(10, 10, 16, 80).game_loop()
